75c3e9212c75936443bb1120965f8342108f17d3
RC <rc.chandru@gmail.com>
2016-10-11 19:39:41 +0530

    Realignment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,9 @@ class WelcomeController < ApplicationController
 
 	def show
 		if params[:id] == 'redirect'
-      ajax_redirect_to another_index_path
-    else
-      render text: Faker::Lorem.paragraph
+      		ajax_redirect_to another_index_path
+    	else
+	      render text: Faker::Lorem.paragraph
 		end
 	end
 
